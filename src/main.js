b60e5df32089541d8980e8ef65388043eaea3bd3
dummydevil <ahenssolution@gmail.com>
2023-08-14 16:27:32 +0530

    Partial portfolio

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import '../style/modern_normalize.css';
+import '../style/style.css';
+import '../style/components/header.css';
+import '../style/components/hero.css';
+import '../style/components/about.css';
+import '../style/utilities.css';
+
+
