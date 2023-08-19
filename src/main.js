bc828727077ff224e1cdace81c2aace73b3c05a4
dummydevil <ahenssolution@gmail.com>
2023-08-19 21:26:38 +0530

    commit

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import '../style/style.css';
 import '../style/components/header.css';
 import '../style/components/hero.css';
 import '../style/components/about.css';
+import '../style/components/featured.css';
+import '../style/components/work.css';
+import '../style/components/contact.css';
 import '../style/utilities.css';
 
 
